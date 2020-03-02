7a722d37adce92d48587267ee27f7bbf0cc14f08
arjun.kapur@revolutionit.com.au <arjun.kapur@revolutionit.com.au>
2020-03-02 16:38:32 +1100

    updates

---
+++ extentreports-dotnet-core/Core/RazorEngineTemplate.cs
@@ -0,0 +1,13 @@
+﻿using RazorEngine.Templating;
+
+namespace AventStack.ExtentReports.Core
+{
+    public class RazorEngineTemplate<T> : TemplateBase<T>
+    {
+        public new T Model
+        {
+            get { return base.Model; }
+            set { base.Model = value; }
+        }
+    }
+}
